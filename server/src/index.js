7bed4ed8cb6af90aee1254992310dfc51dff1b78
Fabio Hsu <Fabio90h@fabios-mbp.attlocal.net>
2020-04-17 06:42:17 -0700

    Adding comment to mongo setup

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
+/**
+ * Go to cloud.mongodb.com to sign up form the database host
+ */
 const { mongoUser, mongoPassword } = require("../.config/mongoInfo");
 const monogoURI = `mongodb+srv://${mongoUser}:${mongoPassword}@cluster0-xdvc0.mongodb.net/test?retryWrites=true&w=majority`;
 mongoose.connect(monogoURI, {
