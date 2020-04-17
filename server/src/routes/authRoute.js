aa9aa6760b5116266e1ba94018da797c98a110af
Fabio Hsu <Fabio90h@fabios-mbp.attlocal.net>
2020-04-17 08:36:48 -0700

    Handle Error For Auth Request

--- server/src/routes/authRoute.js
+++ server/src/routes/authRoute.js
@@ -7,9 +7,13 @@ const router = express.Router();
 
 router.post("/signup", async (req, res) => {
 	let { email, password } = req.body;
-	let user = new User({ email, password });
-	await user.save();
-	res.send("You made a post request");
+	try {
+		let user = new User({ email, password });
+		await user.save();
+		res.send("You made a post request");
+	} catch (error) {
+		res.status(422).send(error.message);
+	}
 });
 
 module.exports = router;
