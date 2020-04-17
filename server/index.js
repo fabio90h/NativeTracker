c90812c8366cd11f7eaf4c908ec4a9d5cc71515f
Fabio Hsu <Fabio90h@fabios-mbp.attlocal.net>
2020-04-17 06:38:03 -0700

    Adding mongoDb connection

--- server/index.js
+++ server/index.js
@@ -1,4 +1,19 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
+const { mongoUser, mongoPassword } = require("./.config/mongoInfo");
+const monogoURI = `mongodb+srv://${mongoUser}:${mongoPassword}@cluster0-xdvc0.mongodb.net/test?retryWrites=true&w=majority`;
+mongoose.connect(monogoURI, {
+	useUnifiedTopology: true,
+	useNewUrlParser: true,
+});
+const db = mongoose.connection;
+db.on("connected", () => {
+	console.log("Connect to MongoDB");
+});
+db.on("error", (error) => {
+	console.log("There was an error connecting to MongoDB", error);
+});
 
 const app = express();
 
