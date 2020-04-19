a5d02d2dfec001319eaf868c7dff98f8f9bdc40e
Fabio Hsu <Fabio90h@fabios-mbp.attlocal.net>
2020-04-18 17:36:12 -0700

    Enable the ability to add new tracks

--- server/src/routes/trackRoute.js
+++ server/src/routes/trackRoute.js
@@ -9,10 +9,25 @@ const Track = mongoose.model("Track");
 // User need to be signed in inorder to perform route
 router.use(reqAuth);
 
+// Get all tracks
 router.get("/tracks", async (req, res) => {
 	let track = await Track.find({ userId: req.user._id });
 
 	res.send(track);
+});
+
+// Create a new track
+router.post("/tracks", async (req, res) => {
+	const { name, locations } = req.body;
+
+	if (!name || !locations) res.status(422).send({ error: " you must provde a name and locations" });
+	try {
+		const track = new Track({ name, locations, userId: req.user._id });
+		await track.save();
+		res.send(track);
+	} catch (err) {
+		res.send(422).send({ error: err.message });
+	}
 });
 
 module.exports = router;
