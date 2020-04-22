471c5c5e32a3376ff172b46d2ce95c4caf8b8055
Fabio Hsu <Fabio90h@fabios-mbp.attlocal.net>
2020-04-22 08:20:00 -0700

    Saving token and dispatching signup

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,9 +1,12 @@
 import createDataContext from "./createDataContext";
 import trackerAPI from "../api/tracker";
+import { AsyncStorage } from "react-native";
 
 // Declare Reducer
 const authReducer = (state, action) => {
 	switch (action.type) {
+		case "signup":
+			return { errorMessage: "", token: action.payload };
 		case "add_error":
 			return { ...state, errorMessage: action.payload };
 		default:
@@ -18,7 +21,8 @@ const signUp = (dispatch) => async ({ email, password }) => {
 			email,
 			password,
 		});
-		console.log(response);
+		await AsyncStorage.setItem("token", response.data.token);
+		dispatch({ type: "signup", payload: response.data.token });
 	} catch (error) {
 		dispatch({
 			type: "add_error",
