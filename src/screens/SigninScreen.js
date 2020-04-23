066138213241b7d1bdc33473f4e9eaf980751368
Fabio Hsu <Fabio90h@fabios-mbp.attlocal.net>
2020-04-23 07:30:02 -0700

    Adding Sign in screen components

--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Text, View, StyleSheet, Button } from "react-native";
+import AuthForm from "../components/AuthForm";
+import { Context as AuthContext } from "../context/AuthContext";
+import NavLink from "../components/NavLink";
 
 const SigninScreen = ({ navigation }) => {
+	const { state, signIn } = useContext(AuthContext);
 	return (
-		<>
-			<Text>SigninScreen</Text>
-			<Button title="Sign in" onPress={() => navigation.navigate("mainFlow")} />
-		</>
+		<View style={styles.container}>
+			<AuthForm
+				title="Sign in Tracker"
+				errorMessage={state.errorMessage}
+				onSubmit={signIn}
+				onSubmitText="Sign In"
+			/>
+			<NavLink linkText="Dont have an Account? Sign up here." navigateText="Signup" />
+		</View>
 	);
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: "center",
+		marginBottom: 150,
+	},
+});
 
 export default SigninScreen;
