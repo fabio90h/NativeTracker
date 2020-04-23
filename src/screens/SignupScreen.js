2808c4743a7a5335a1a9e9f7afb234ff9baa8a9b
Fabio Hsu <Fabio90h@fabios-mbp.attlocal.net>
2020-04-23 06:27:43 -0700

    Refactoring SignUp Screen with reusable components

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,55 +1,21 @@
-import React, { useState, useContext } from "react";
+import React, { useContext } from "react";
 import { StyleSheet, View, TouchableOpacity } from "react-native";
-import { Button, Input, Text } from "react-native-elements";
-import Spacer from "../components/Spacer";
 import { Context as AuthContext } from "../context/AuthContext";
+import AuthForm from "../components/AuthForm";
+import NavLink from "../components/NavLink";
 
 const SignupScreen = ({ navigation }) => {
 	const { state, signUp } = useContext(AuthContext);
-	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
 
 	return (
 		<View style={styles.container}>
-			<Spacer>
-				<Text style={{ textAlign: "center" }} h3>
-					Sign Up for Tracker
-				</Text>
-				<Spacer />
-			</Spacer>
-			<Spacer>
-				<Input
-					label="Email"
-					value={email}
-					onChangeText={setEmail}
-					autoCapitalize={"none"}
-					autoCorrect={false}
-				/>
-			</Spacer>
-			<Spacer>
-				<Input
-					label="Password"
-					value={password}
-					onChangeText={setPassword}
-					secureTextEntry
-					autoCapitalize={"none"}
-					autoCorrect={false}
-				/>
-			</Spacer>
-			{/* Error Message */}
-			{state.errorMessage ? (
-				<Spacer>
-					<Text style={styles.errorMessageStyle}>{state.errorMessage}</Text>
-				</Spacer>
-			) : null}
-			<Spacer>
-				<Button title="Sign Up" onPress={() => signUp({ email, password })} />
-			</Spacer>
-			<Spacer>
-				<TouchableOpacity onPress={() => navigation.navigate("Signin")}>
-					<Text style={styles.signInStyle}>Already have an account? Sign In here.</Text>
-				</TouchableOpacity>
-			</Spacer>
+			<AuthForm
+				title="Sign Up for Tracker"
+				errorMessage={state.errorMessage}
+				onSubmit={signUp}
+				onSubmitText="Sign Up"
+			/>
+			<NavLink navigateText="Signin" linkText="Alread Have an Account. Sign in instead." />
 		</View>
 	);
 };
@@ -59,11 +25,6 @@ const styles = StyleSheet.create({
 		flex: 1,
 		justifyContent: "center",
 		marginBottom: 150,
-	},
-	errorMessageStyle: {
-		color: "red",
-		textAlign: "center",
-		fontSize: 16,
 	},
 	signInStyle: {
 		color: "blue",
