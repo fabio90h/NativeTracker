69d07d920d1288ccabf9d7ebafd874b87cb9ab16
Fabio Hsu <Fabio90h@fabios-mbp.attlocal.net>
2020-04-26 12:58:54 -0700

    Adding Reading Location to TrackCreateScreen

--- src/components/Map.js
+++ src/components/Map.js
@@ -2,33 +2,14 @@ import React, { useEffect, useState } from "react";
 import { Text, StyleSheet } from "react-native";
 
 import MapView from "react-native-maps";
-import { requestPermissionsAsync } from "expo-location";
 
 const Map = () => {
-	const [mapError, setMapError] = useState(null);
-
-	const startWatching = async () => {
-		try {
-			let permission = await requestPermissionsAsync();
-			if (permission.status === "denied") {
-				setMapError("You must enable locations to use this feature");
-			}
-		} catch (error) {
-			setMapError(error);
-		}
-	};
-
-	useEffect(() => {
-		startWatching();
-	});
-
 	return (
 		<>
 			<MapView
 				style={styles.map}
 				initialRegion={{ latitude: 38, longitude: -122, latitudeDelta: 0.01, longitudeDelta: 0.01 }}
 			/>
-			{mapError ? <Text>{mapError}</Text> : null}
 		</>
 	);
 };
--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,14 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Text, View, StyleSheet } from "react-native";
 import Map from "../components/Map";
 import { SafeAreaView } from "react-native-safe-area-context";
-import "../_mockLocation";
+import { requestPermissionsAsync, watchPositionAsync, Accuracy } from "expo-location";
+//import "../_mockLocation";
 
 const TrackCreateScreen = () => {
+	const [mapError, setMapError] = useState(null);
+
+	const startWatching = async () => {
+		try {
+			let permission = await requestPermissionsAsync();
+			if (permission.status === "denied") {
+				setMapError("You must enable locations to use this feature");
+			}
+			await watchPositionAsync(
+				{
+					accuracy: Accuracy.BestForNavigation,
+					timeInterval: 1000,
+					distanceInterval: 10,
+				},
+				(location) => console.log(location)
+			);
+		} catch (error) {
+			setMapError(error);
+		}
+	};
+
+	useEffect(() => {
+		startWatching();
+	});
 	return (
 		<SafeAreaView>
 			<Text>TrackCreateScreen</Text>
 			<Map />
+			{mapError ? <Text>{mapError}</Text> : null}
 		</SafeAreaView>
 	);
 };
