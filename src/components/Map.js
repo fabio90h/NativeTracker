92edb62758bce2609df308fcf0f15f927c026650
Fabio Hsu <Fabio90h@fabios-mbp.attlocal.net>
2020-04-29 07:37:24 -0700

    Drawing Line to track path of locations

--- src/components/Map.js
+++ src/components/Map.js
@@ -2,11 +2,11 @@ import React, { useContext } from "react";
 import { Text, StyleSheet, ActivityIndicator } from "react-native";
 import { Context as LocationContext } from "../context/LocationContext";
 
-import MapView, { Circle } from "react-native-maps";
+import MapView, { Circle, Polyline } from "react-native-maps";
 
 const Map = () => {
 	const {
-		state: { currentLocation },
+		state: { currentLocation, locations },
 	} = useContext(LocationContext);
 
 	if (!currentLocation) {
@@ -25,6 +25,7 @@ const Map = () => {
 					strokeColor="rgba(158,158,255,1.0)"
 					fillColor="rgba(158,158,255,0.3)"
 				/>
+				<Polyline coordinates={locations.map((location) => location.coords)} />
 			</MapView>
 		</>
 	);
