43f1ad181c3adf028d0c682ff3dcf86beda5acca
Fabio Hsu <Fabio90h@fabios-mbp.attlocal.net>
2020-04-30 07:54:33 -0700

    Adding FetchTracks from DB

--- src/context/TrackContext.js
+++ src/context/TrackContext.js
@@ -4,17 +4,25 @@ import tracker from "../api/tracker";
 // Reducer
 const trackReducer = (state, action) => {
 	switch (action.type) {
+		case "fetch_tracks":
+			return action.payload;
 		default:
 			return state;
 	}
 };
 
 // Actions
-const fetchTracks = (dispatch) => () => {};
+const fetchTracks = (dispatch) => async () => {
+	try {
+		let response = await tracker.get("/tracks");
+		dispatch({ type: "fetch_tracks", payload: response.data });
+	} catch (error) {
+		console.error(error);
+	}
+};
 const saveTrack = (dispatch) => async (name, locations) => {
 	try {
 		let response = await tracker.post("/tracks", { name, locations });
-		console.log("track saved", response.config.data);
 	} catch (error) {
 		console.error(error);
 	}
