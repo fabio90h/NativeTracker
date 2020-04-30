c13ffe0985a652742dfa9108560676e048c28c36
Fabio Hsu <Fabio90h@fabios-mbp.attlocal.net>
2020-04-30 08:42:01 -0700

    Fetching all Tracks in TrackListScreen

--- src/screens/TrackListScreen.js
+++ src/screens/TrackListScreen.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Text, View, StyleSheet, Button } from "react-native";
+import { Context as TrackContext } from "../context/TrackContext";
 
 const TrackListScreen = ({ navigation }) => {
+	const { state, fetchTracks } = useContext(TrackContext);
+	navigation.addListener("focus", fetchTracks);
+	console.log(state);
 	return (
 		<>
 			<Text>TrackListScreen</Text>
