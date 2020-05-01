0f05ae71a97e04fe8ca95cd3f5d1b9a1ec3d561a
Fabio Hsu <Fabio90h@fabios-mbp.attlocal.net>
2020-05-01 14:04:59 -0700

    Show Track in a Map

--- src/screens/TrackDetailScreen.js
+++ src/screens/TrackDetailScreen.js
@@ -1,18 +1,29 @@
 import React, { useContext } from "react";
-import { Text, View, StyleSheet } from "react-native";
+import { Text, StyleSheet } from "react-native";
+import MapView, { Polyline } from "react-native-maps";
 import { Context as TrackContext } from "../context/TrackContext";
 
 const TrackDetailScreen = ({ route }) => {
 	const { state } = useContext(TrackContext);
 	const track = state.find((current) => current._id === route.params._id);
-	console.log(track.name);
+	const initialCoords = track.locations[0].coords;
 	return (
 		<>
-			<Text>TrackDetailScreen</Text>
+			<MapView
+				style={styles.map}
+				initialRegion={{ longitudeDelta: 0.01, latitudeDelta: 0.01, ...initialCoords }}
+			>
+				<Polyline coordinates={track.locations.map((location) => location.coords)} />
+			</MapView>
 		</>
 	);
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	map: {
+		width: "auto",
+		height: 350,
+	},
+});
 
 export default TrackDetailScreen;
